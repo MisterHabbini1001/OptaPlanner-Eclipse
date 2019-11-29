d8bea261480e0a1b781d77972c871ee206be213d
H. Gaastra <0910681@hr.nl@example.com>
2019-11-29 09:16:11 +0100

    OptaPlanner initial version

---
+++ optaplanner standard/src/main/java/org/optaplanner/examples/scrabble/app/TestSLF4J.java
@@ -0,0 +1,12 @@
+package org.optaplanner.examples.scrabble.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestSLF4J {
+	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(TestSLF4J.class);
+		logger.info("Hello World");
+	}
+}
+
